8f08d911d379d7c48fc7a60731823478876d4cd9
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 21:38:33 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	return 0;
+void addRowToEnd(int**& array, int& numRows, int numCols, int* newRow){
+    int newNumRows = numRows + 1;
+    int** newArray = new int* [newNumRows];
+
+    for (int i = 0; i < numRows; ++i) {
+        newArray[i] = new int[numCols];
+        for (int j = 0; j < numCols; ++j) {
+            newArray[i][j] = array[i][j];
+        }
+    }
+
+    newArray[numRows] = newRow;
+
+    for (int i = 0; i < numRows; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+    array = newArray;
+    numRows = newNumRows;
 }
 
+int main() {
+    const int numCols = 3;  
+    int numRows = 2;        
+    int** myArray = new int* [numRows];
+    for (int i = 0; i < numRows; ++i) {
+        myArray[i] = new int[numCols];
+        for (int j = 0; j < numCols; ++j) {
+            myArray[i][j] = i * numCols + j + 1;
+        }
+    }
+
+    cout << "Initial array:" << endl;
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            cout << myArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int* newRow = new int[numCols] {7, 8, 9};
+    addRowToEnd(myArray, numRows, numCols, newRow);
+
+    cout << "\nArray after adding row:" << endl;
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            cout << myArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < numRows; ++i) {
+        delete[] myArray[i];
+    }
+    delete[] myArray;
+    delete[] newRow;
+
+    return 0;
+}
 
